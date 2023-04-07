04dbd3870434ac416201e72b8770555f12a57b80
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-04-07 13:34:35 +0300

    Guarantee received only one error

--- Modules/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ Modules/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -36,13 +36,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        var capturedError: RemoteFeedLoader.Error?
-        client.error = NSError(domain: "Test", code: 0)
-        sut.load { error in
-            capturedError = error
-        }
         
-        XCTAssertEqual(capturedError, .connectivity)
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load { capturedError.append($0) }
+        
+        XCTAssertEqual(capturedError, [.connectivity])
     }
 
     // MARK: - Helpers
