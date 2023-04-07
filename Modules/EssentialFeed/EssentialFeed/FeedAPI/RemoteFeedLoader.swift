43f5b33ef30a50594f4ddc509faa9d99579d6832
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-04-07 16:30:28 +0300

    Moved mapping logic to FeedItemsMapper

--- Modules/EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ Modules/EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -29,13 +29,12 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
+                if let items = try? FeedItemsMapper.map(data, response) {
+                    completion(.success(items))
                 } else {
-                    completion(.failure(.invalidData ))
+                    completion(.failure(.invalidData))
                 }
-                
-            case let .failure(error):
+            case .failure:
                 completion(.failure(.connectivity))
             }
         }
@@ -54,13 +53,27 @@ extension RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-private struct Item: Equatable, Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    private struct Item: Equatable, Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var feedItem: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        
+        return try JSONDecoder().decode(Root.self, from: data).items.map(\.feedItem)
+    }
 }
