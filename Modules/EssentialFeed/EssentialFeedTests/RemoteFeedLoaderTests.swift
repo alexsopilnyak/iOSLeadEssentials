5219d905fc398ffe9d7535e3a0bb986a996f89b6
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-04-07 13:33:08 +0300

    Deliver connectivity error on client

--- Modules/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ Modules/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,7 +9,6 @@ import XCTest
 import EssentialFeed
 
 final class RemoteFeedLoaderTests: XCTestCase {
-    
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         
@@ -34,13 +33,28 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        var capturedError: RemoteFeedLoader.Error?
+        client.error = NSError(domain: "Test", code: 0)
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
 
     // MARK: - Helpers
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
