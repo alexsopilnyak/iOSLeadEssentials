14be78cf78cb76dc2ab6874f361a6e4a749b5d91
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-04-08 10:37:53 +0300

    Move mapping loci to Mapper

--- Modules/EssentialFeed/EssentialFeed/FeedAPI/FeedApiMapper.swift
+++ Modules/EssentialFeed/EssentialFeed/FeedAPI/FeedApiMapper.swift
@@ -8,11 +8,30 @@
 import Foundation
 
 final class FeedItemsMapper {
-    private struct Root: Decodable {
+    private static var OK_200: Int { 200 }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard
+            response.statusCode == OK_200,
+            let root = try? JSONDecoder().decode(Root.self, from: data)
+        else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+        
+        return .success(root.feed)
+    }
+}
+
+private extension FeedItemsMapper {
+    struct Root: Decodable {
         let items: [Item]
+        
+        var feed: [FeedItem] {
+            items.map(\.feedItem)
+        }
     }
 
-    private struct Item: Equatable, Decodable {
+    struct Item: Equatable, Decodable {
         let id: UUID
         let description: String?
         let location: String?
@@ -21,16 +40,6 @@ final class FeedItemsMapper {
         var feedItem: FeedItem {
             FeedItem(id: id, description: description, location: location, imageURL: image)
         }
-    }
-    
-    private static var OK_200: Int { 200 }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        
-        return try JSONDecoder().decode(Root.self, from: data).items.map(\.feedItem)
     }
 }
 
