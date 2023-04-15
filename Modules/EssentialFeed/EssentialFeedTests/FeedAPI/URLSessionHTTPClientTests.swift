15d779accfdb946fedb12501502682327e26fac8
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-04-15 18:46:09 +0300

    Make sut helper factory method

--- Modules/EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ Modules/EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -26,7 +26,6 @@ class URLSessionHTTPClient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
-    
     override class func setUp() {
         URLProtocolStub.startInterceptingRequests()
     }
@@ -38,13 +37,14 @@ final class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_PerformsGETRequestWithURL() {
         let url = URL(string: "https://any-url.com")!
         let exp = expectation(description: "Wait for request")
+        
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -52,12 +52,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
     func test_getFormURL_failsOnRequestError() {
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "Any error", code: 1)
-        let sut = URLSessionHTTPClient()
         
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         let exp = expectation(description: "Waint for an error")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -73,6 +72,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     // MARK: - Helpers
+    
+    func makeSUT() -> URLSessionHTTPClient {
+        URLSessionHTTPClient()
+    }
    
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
