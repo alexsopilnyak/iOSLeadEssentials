52cc97574237d7d788fb62f5483234224702ae29
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-05-02 19:17:01 +0300

    Delivering error when retrieval operation fails

--- EssentialFeed/EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -19,9 +19,27 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
     func test_load_requestsCahceRetrieval() {
         let (sut, store) = makeSUT()
         
-        sut.load()
+        sut.load() { _ in }
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        let retrievalError = anyNSError()
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedError: Error?
+        sut.load() { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeRetrieval(with: retrievalError)
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual((receivedError as NSError?)?.domain, retrievalError.domain)
+        XCTAssertEqual((receivedError as NSError?)?.code, retrievalError.code)
     }
     
 }
@@ -42,4 +60,6 @@ private extension LoadFeedFromCacheUseCaseTests {
         
         return (sut, store)
     }
+    
+    func anyNSError() -> NSError { NSError(domain: "any error", code: 0) }
 }
