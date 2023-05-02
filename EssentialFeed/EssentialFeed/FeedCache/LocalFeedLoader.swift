ccd6e01f970222d3d50bd22b98004c3e2f409d12
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-05-02 22:06:35 +0300

    Extract duplicate test code into shared helper method

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -35,7 +35,7 @@ public class LocalFeedLoader {
             if let error {
                 completion(.failure(error))
             } else {
-                completion(.success([]))
+                completion(.success([ ]))
             }
             
         }
