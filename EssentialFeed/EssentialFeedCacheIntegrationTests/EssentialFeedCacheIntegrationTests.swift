3a1c6e76e1512fd4d29363d96afe3fee2b490d44
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-05-24 21:35:14 +0300

    `LocalFeedLoader` in integration with `CoreDataFeedStore` delivers items saved on separated instances

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -35,6 +35,33 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "Waiting for save")
+        sutToPerformSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "Waiting for load")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(loadedFeed):
+                XCTAssertEqual(loadedFeed, feed)
+            case let .failure(error):
+                XCTFail("Expected success result, gor an \(error) instead")
+            }
+            
+            loadExp.fulfill()
+        }
+        wait(for: [loadExp], timeout: 1.0)
+    }
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
