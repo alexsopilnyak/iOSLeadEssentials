e8285f22414840657706248d892d3c316e136ea5
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-08-27 09:23:53 +0300

    Remove repeating code to helper makeSUT with memory leaks taking

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -27,18 +27,29 @@ final class FeedViewController: UIViewController {
 final class FeedViewControllerTests: XCTestCase {
 
     func test_init_doesNotLoadFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
+        let (_, loader) = makeSUT()
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
     func test_viewDidLoad_loadsFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
+        let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
+    private func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        trackForMemoryLeaks(loader)
+        trackForMemoryLeaks(sut)
+        
+        return (sut, loader)
     }
     
     final class LoaderSpy: FeedLoader {
