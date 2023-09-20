9b02b4ff39610ef521dd9c5a0e5a8f75f64c65c7
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-09-20 12:32:55 +0300

    Format access control order following conventions

--- EssentialFeed/EssentialFeediOS/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Controllers/FeedRefreshViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import EssentialFeed
 
-final class FeedRefreshViewController: NSObject {
+public final class FeedRefreshViewController: NSObject {
     private(set) lazy var view: UIRefreshControl = {
         let view = UIRefreshControl()
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
