70e71459aed2a5798355819a4c4670560ebe73eb
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-09-20 12:46:40 +0300

    Extract FeedImageCell creation/configuration and image loading logic to FeedImageCellController

---
+++ EssentialFeed/EssentialFeediOS/Controllers/FeedImageCellController.swift
@@ -0,0 +1,56 @@
+//
+//  FeedImageCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Oleksandr Sopilniak on 20.09.2023.
+//
+
+import UIKit
+import EssentialFeed
+
+final class FeedImageCellController {
+    private var task: FeedImageDataLoaderTask?
+    
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func view() -> UITableViewCell {
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (model.location == nil)
+        cell.locationLabel.text = model.location
+        cell.descriptionLabel.text = model.description
+        cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
+        cell.feedImageContainer.startShimmering()
+        
+        let loadImage = { [weak self, weak cell] in
+            guard let self else { return }
+            
+            self.task = imageLoader.loadImageData(from: model.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = image != nil
+                cell?.feedImageContainer.stopShimmering()
+            }
+        }
+        
+        loadImage()
+        cell.onRetry = loadImage
+        
+        return cell
+    }
+    
+    func preload() {
+        task = imageLoader.loadImageData(from: model.url) { _ in }
+    }
+    
+    deinit {
+        task?.cancel()
+    }
+}
