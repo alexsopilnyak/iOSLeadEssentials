bf0946c4364078f51decd770d1df3b3d77bf8087
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-09-29 10:59:03 +0300

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Oleksandr Sopilniak on 29.09.2023.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
