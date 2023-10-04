c3f586b59c67b7e31ac4fbad51a862446f59fc46
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-10-04 22:38:56 +0300

    Configure EssentialApp scheme to execute tests in random order and gather coverage for the EssentialApp target

---
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Oleksandr Sopilniak on 04.10.2023.
+//
+
+import XCTest
+@testable import EssentialApp
+
+final class EssentialAppTests: XCTestCase {
+
+
+}
