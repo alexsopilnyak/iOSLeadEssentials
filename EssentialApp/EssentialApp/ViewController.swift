5547c0f7199a39241b48bca02a6a8d68e57715be
Oleksandr Sopilniak <sopilnyak.alexandr@gmail.com>
2023-10-04 22:03:24 +0300

    Added empty EssentialFeedApp

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Oleksandr Sopilniak on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
